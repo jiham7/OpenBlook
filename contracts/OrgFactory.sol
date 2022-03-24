097b42b5c0084aaf292e7d4efce2c90c7069a139
Jiham Lee <jiham7@gmail.com>
2022-03-24 18:54:57 -0500

    cleanup

--- contracts/OrgFactory.sol
+++ contracts/OrgFactory.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./Organization.sol";
 contract OrgFactory{
-    Organization[] public organizations;
+    mapping(uint256 => Organization) public organizations;
+    mapping(uint256 => address) public addresses;
     uint256 public orgIdCounter;
 
     event OrgnizationCreated(address orgAddress, uint256 id, string name);
@@ -11,9 +12,14 @@ contract OrgFactory{
         uint256 _id = orgIdCounter;
         orgIdCounter++;
         Organization organization = new Organization(_id, _name);
-        organizations.push(organization);
+        organizations[_id] = organization;
+        addresses[_id] = address(organization);
         emit OrgnizationCreated(address(organization), _id, _name);
         return address(organization);
+    }
+
+    function getOrgAddress(uint256 _id) public view returns(address){
+        return addresses[_id];
     }
  
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,8 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 
 async function main() {
   const [owner, addr1, addr2] = await ethers.getSigners();
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
 
   // Factory Contract Creation
   const OrganizationFactory = await hre.ethers.getContractFactory("OrgFactory");
@@ -25,17 +12,19 @@ async function main() {
   await orgFactory.deployed();
   console.log("Org Factory contract deployed to: ", orgFactory.address);
 
-  //Org Contract Creation
-  // const orgAddress = await orgFactory.createOrganization("ChainshotOrg");
-  // console.log("Organization contract deployed to: ", orgAddress);
-
-  const Organization = await hre.ethers.getContractFactory("Organization");
-  const org = await Organization.deploy(1, "ChainShot");
+  // Creator
+  const ownerSigner = await ethers.getSigner(owner.address);
+  const factoryContract = await ethers.getContractAt('OrgFactory', orgFactory.address, ownerSigner);
   
-  await org.deployed();
-  console.log("Org contract deployed to: ", org.address);
+  // Create new Organization from Factory
+  const newOrg = await factoryContract.createOrganization("ChainshotOrg");
+  const newAddress = await factoryContract.getOrgAddress(0);
 
+  console.log("Organization contract deployed to: " + newAddress);
 
+  const orgContract = await ethers.getContractAt('Organization', newAddress, ownerSigner);
+
+  // Transaction parameters
   const _orgId = 2;
   const _approvers = [addr1.address, addr2.address];
   const _from = '0x70997970c51812dc3a010c7d01b50e0d17dc79c8';
@@ -44,31 +33,33 @@ async function main() {
   const _description = "testing desc";
   const _ipfsHashes = ['testImage', 'testImage2', 'testImage3'];
 
-  const newTxn = await org.createFinanceTxn(_approvers, _from, _to, _amount, _description, _ipfsHashes);
-  // console.log(newTxn);
+  // New Transaction Created
+  const newTxn = await orgContract.createFinanceTxn(_approvers, _from, _to, _amount, _description, _ipfsHashes);
+  console.log("New Transaction created with id 0 ");
+  console.log("Approvers are: " + addr1.address + ", " + addr2.address)
 
   // First approver
   const signer1 = await ethers.getSigner(addr1.address);
-  const myContract = await ethers.getContractAt('Organization', org.address, signer1);
+  const myContract = await ethers.getContractAt('Organization', newAddress, signer1);
   const checkStatus1 = await myContract.getTxnStatus(0);
-  console.log(checkStatus1);
+  console.log("Current Status:" + checkStatus1);
   const out = await myContract.approveTransaction(0);
+  console.log("Txn approved by " + addr1.address);
+  
+  // Attempt to change Status. Not going to work
+  console.log("Attemping to change Status to InProgress");
+  const changeStatus1 = await orgContract.changeTxnStatus(0, "InProgress");
   
   // Second Approver
   const signer2 = await ethers.getSigner(addr2.address);
-  const myContract2 = await ethers.getContractAt('Organization', org.address, signer1);
+  const myContract2 = await ethers.getContractAt('Organization', newAddress, signer1);
   const out2 = await myContract2.approveTransaction(0);
+  console.log("Txn approved by " + addr1.address);
+  console.log("Attemping to change Status to InProgress");
 
-  const changeStatus = await org.changeTxnStatus(0, "InProgress");
-
-  const checkStatus2 = await myContract2.getTxnStatus(0);
-  console.log(checkStatus2);
-
-
-  
-
-  // const trxCount = await org.getTrxNumber();
-  // console.log("Trx count: " + trxCount);
+  // Change Status
+  const checkStatus2 = await orgContract.changeTxnStatus(0, "InProgress");
+  console.log("Current Status:" + checkStatus2);
 
 }
 
